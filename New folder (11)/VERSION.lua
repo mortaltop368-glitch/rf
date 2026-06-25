cc96c3315f6af95b87cdc8559977ef1dae9ef931
mortaltop368-glitch <mortaltop368@gmail.com>
2026-06-25 14:43:44 +0300

    Add files via upload

---
+++ New folder (11)/VERSION.lua
@@ -0,0 +1,6 @@
+-- Saved by UniversalSynSaveInstance (Join to Copy Games) https://discord.gg/wx4ThpAsmw
+
+-- Bytecode (Base64):
+-- CQMAAAEAAAABAgACowAAAIIAAQAAAAEAARgAAAEAAAAAAA==
+
+-- Decompiling is disabled
