9d632c7b7863d6a7293125186584500e5613562a
mortaltop368-glitch <mortaltop368@gmail.com>
2026-06-25 14:43:00 +0300

    Add files via upload

---
+++ New folder (10)/Show Your Pets.lua
@@ -0,0 +1,6 @@
+-- Saved by UniversalSynSaveInstance (Join to Copy Games) https://discord.gg/wx4ThpAsmw
+
+-- Bytecode (Base64):
+-- CQMBDFNob3dZb3VyUGV0cwACAQEAAAAAAyoAAQABAACAggABAAAAAwEBGAAAAwQAAAAAAQAAAQIABaMAAAD/AAAAAAAAAMAAAACCAAIAAQYAAQABAAEYAAAAAgYBAAAAAAE=
+
+-- Decompiling is disabled
